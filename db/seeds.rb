3d74bee6364e26c77c4cb059a69a4d65c6037dbd
MNDMMstudy <107236809+MNDMMstudy@users.noreply.github.com>
2022-09-24 12:23:51 +0000

    seeds追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,50 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 Admin.create!(
-   email: 'admin@admin',
-   password: 'adminadmin'
+  email: 'admin@test.com',
+  password: 'password'
+)
+
+User.create!(
+  [
+    {email: 'satou@test.com', name: '佐藤', password: 'password'},
+    {email: 'suzuki@test.com', name: '鈴木', password: 'password'},
+    {email: 'takahasi@test.com', name: '高橋', password: 'password'}
+  ]
+)
+
+YouteGenre.create!(
+  [
+    {name: '褒めてください'},
+    {name: 'アドバイス求む'}
+  ]
+)
+
+CommentGenre.create!(
+  [
+    {name: 'いいね！'},
+    {name: 'アドバイス'}
+  ]
+)
+
+users = User.all
+youte_genres = YouteGenre.all
+comment_genres = CommentGenre.all
+youtes = Youte.all
+
+Youte.create!(
+  [
+    {text: '商談がうまくいきました。', star: '1', user_id: users[0].id, youte_genre_id: youte_genres[0].id },
+    {text: 'プログラミングの学習方法でいい方法はありますか？', star: '5', youte_genre_id: youte_genres[1].id, user_id: users[1].id },
+    {text: '自炊してお弁当を持ってくることができました！', star: '3', youte_genre_id: youte_genres[0].id, user_id: users[2].id }
+  ]
+)
+
+Comment.create!(
+  [
+    {text: 'お疲れ様です！', youte_id: youtes[0].id, genre_id: comment_genres[0].id, user_id: users[1].id },
+    {text: 'HTML/CSS教本という本がおすすめです。', youte_id: youtes[1].id, genre_id: comment_genres[1].id, user_id: users[2].id  },
+    {text: '勉強する時間を決めておくことが大切です。', youte_id: youtes[1].id, genre_id: comment_genres[1].id, user_id: users[0].id  },
+    {text: 'お仕事も頑張って、お弁当作ってえらい！', youte_id: youtes[2].id, genre_id: comment_genres[0].id, user_id: users[0].id  }
+  ]
 )
